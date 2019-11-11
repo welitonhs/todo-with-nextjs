a4ee9ac653c857982ad26ae5fc753519b3f385e8
welitonhs <weliton.sernajotto23@gmail.com>
2019-11-11 00:30:47 -0300

    Initial Commit

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+
+const About = () => (
+  <div>
+    <Link href="/">
+      <a title="Home Page">Home Page</a>
+    </Link>
+    <p>About Page</p>
+  </div>
+)
+
+export default About
